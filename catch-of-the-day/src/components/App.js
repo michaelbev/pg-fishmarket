dc365871fdec6e786eb389665c2cc1c0a46b2106
Michael Bevilacqua <mebevilacqua@hotmail.com>
2022-10-20 19:56:17 -0600

    HTML with JSX

---
+++ catch-of-the-day/src/components/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="catch-of-the-day">
+        <div className="menu">
+          <header tagline="Fresh Seafood Market" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
