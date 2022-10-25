142893d138d5a2a43175a4868ea8a5a7d8e59402
Michael Bevilacqua <mebevilacqua@hotmail.com>
2022-10-25 12:20:35 -0600

    Finished order jira PFM-2433

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -1,10 +1,38 @@
 import React from 'react'
+import { formatPrice } from '../helpers'
 
 class Order extends React.Component {
-  render() {
+  renderOrder = key => {
+    const fish = this.props.fishes[key]
+    const count = this.props.order[key]
+    const isAvailable = fish && fish.status === 'available'
+    if (!isAvailable)
+      return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>
     return (
-      <div className="order">
-        <h2>Your Order</h2>
+      <li key={key}>
+        {count} lbs {fish.name}
+        {formatPrice(count * fish.price)}
+      </li>
+    )
+  }
+
+  render() {
+    const orderIDs = Object.keys(this.props.order)
+    const total = orderIDs.reduce((prevTotal, key) => {
+      const fish = this.props.fishes[key]
+      const count = this.props.order[key]
+      const isAvailable = fish && fish.status === 'available'
+      if (isAvailable) {
+        return prevTotal + (count * fish.price || 0)
+      }
+      return prevTotal
+    }, 0)
+
+    return (
+      <div className="order-wrap">
+        <h2>Order</h2>
+        <ul className="order">{orderIDs.map(this.renderOrder)}</ul>
+        <div className="total">{formatPrice(total)}</div>
       </div>
     )
   }
