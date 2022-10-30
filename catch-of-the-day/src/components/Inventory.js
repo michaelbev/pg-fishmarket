3caabb61a1552a18111a608bf42a177b33d98f27
Michael Bevilacqua <mebevilacqua@hotmail.com>
2022-10-30 13:34:43 -0600

    adding login page

--- catch-of-the-day/src/components/Inventory.js
+++ catch-of-the-day/src/components/Inventory.js
@@ -2,6 +2,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import AddFishForm from './AddFishForm'
 import EditFishForm from './EditFishForm'
+import Login from './Login'
 
 class Inventory extends React.Component {
   static propTypes = {
@@ -12,7 +13,13 @@ class Inventory extends React.Component {
     loadSampleFishes: PropTypes.func,
   }
 
+  authenticate = provider => {
+    alert(provider)
+    // console.alert('Trying to log in:' + { provider })
+  }
+
   render() {
+    return <Login authenticate={this.authenticate} />
     return (
       <div className="inventory">
         <h2>Inventory</h2>
