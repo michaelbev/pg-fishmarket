e11d6dc68159df626d30a1c3409d1cca2af3d858
Michael Bevilacqua <mebevilacqua@hotmail.com>
2022-11-01 18:16:43 -0600

    added site code for www.mebevilacqua.com

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -7,10 +7,11 @@ const firebaseApp = firebase.initializeApp({
   databaseURL: 'https://catch-of-the-day-a809e.firebaseio.com',
 })
 
-// const appCheck = firebase.appCheck()
+const appCheck = firebase.appCheck()
 // // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
 // // key is the counterpart to the secret key you set in the Firebase console.
-// appCheck.activate('6LeedssiAAAAAI4Jdm_9YpcgelLuAWSBAKVTQQrE', true)
+//appCheck.activate('6LeedssiAAAAAI4Jdm_9YpcgelLuAWSBAKVTQQrE', true)
+appCheck.activate('6LdkFdAiAAAAAMJqmvAWP0dzXjvpUjWsQKRaTiXl', true)
 
 const base = Rebase.createClass(firebaseApp.database())
 
