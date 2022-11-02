049cc5aa677e399acadbfcaab9f5527015cd1fbf
Michael Bevilacqua <mebevilacqua@hotmail.com>
2022-11-01 18:05:16 -0600

    fixed authdomain

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -3,9 +3,14 @@ import firebase from 'firebase/app'
 
 const firebaseApp = firebase.initializeApp({
   apiKey: 'AIzaSyDXUCNSpi5u07F76httlCTXAA4mPVQlEHs',
-  authDomain: 'catch-of-the-day-wes-bos-2.firebaseapp.com',
-  databaseURL: 'https://catch-of-the-day-wes-bos-2.firebaseio.com',
+  authDomain: 'catch-of-the-day-a809e.firebaseapp.com',
+  databaseURL: 'https://catch-of-the-day-a809e.firebaseio.com',
 })
+
+// const appCheck = firebase.appCheck()
+// // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// // key is the counterpart to the secret key you set in the Firebase console.
+// appCheck.activate('6LeedssiAAAAAI4Jdm_9YpcgelLuAWSBAKVTQQrE', true)
 
 const base = Rebase.createClass(firebaseApp.database())
 
