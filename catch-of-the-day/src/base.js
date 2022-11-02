85ffb918d76bf6e4c104449b96c212577c965641
Michael Bevilacqua <mebevilacqua@hotmail.com>
2022-11-02 15:46:36 -0600

    Added TODO for appcheck V3

--- catch-of-the-day/src/base.js
+++ catch-of-the-day/src/base.js
@@ -6,12 +6,12 @@ const firebaseApp = firebase.initializeApp({
   authDomain: 'catch-of-the-day-a809e.firebaseapp.com',
   databaseURL: 'https://catch-of-the-day-a809e.firebaseio.com',
 })
-
-const appCheck = firebase.appCheck()
-// // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
-// // key is the counterpart to the secret key you set in the Firebase console.
-//appCheck.activate('6LeedssiAAAAAI4Jdm_9YpcgelLuAWSBAKVTQQrE', true)
-appCheck.activate('6LdkFdAiAAAAAMJqmvAWP0dzXjvpUjWsQKRaTiXl', true)
+// TODO: Fix this, as app check is not working
+// const appCheck = firebase.appCheck()
+// // // Pass your reCAPTCHA v3 site key (public key) to activate(). Make sure this
+// // // key is the counterpart to the secret key you set in the Firebase console.
+// //appCheck.activate('6LeedssiAAAAAI4Jdm_9YpcgelLuAWSBAKVTQQrE', true)
+// appCheck.activate('6LdkFdAiAAAAAMJqmvAWP0dzXjvpUjWsQKRaTiXl', true)
 
 const base = Rebase.createClass(firebaseApp.database())
 
