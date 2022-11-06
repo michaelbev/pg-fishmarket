f087d3ae2d88796a6d36d3e0924b9fba9cdf7093
Michael Bevilacqua <mebevilacqua@hotmail.com>
2022-11-06 15:55:23 -0700

    Added random challenges files

---
+++ catch-of-the-day/src/components/challanges/SeatChallange.js
@@ -0,0 +1,70 @@
+function ArrayChallenge(arr) {
+  // code goes here
+  let deskCount = arr[0]
+  let seatCombo = []
+  let seatCombos = []
+  const [, ...occupiedDesks] = arr
+  highestSeat = occupiedDesks.reduce((a, b) => Math.max(a, b), -Infinity)
+
+  // toggle for console.log
+  // console.log = function () {}
+
+  console.log('INPUT: ' + JSON.stringify(arr))
+  console.log('deskCount: ' + deskCount)
+  console.log('occupiedDesksCount: ' + occupiedDesks.length)
+  console.log('occupiedDesks: ' + occupiedDesks)
+  console.log('highest desk seat: ' + highestSeat)
+
+  // Test for valid input
+  if (occupiedDesks.length > deskCount || highestSeat > deskCount) {
+    throw new Error('InvalidInput' + JSON.stringify(arr))
+  } else {
+    console.log('--valid arrangement--')
+  }
+
+  //Valid arrangement
+
+  for (let i = 1; i <= deskCount; i++) {
+    // console.log('\nEVALUATING seat (' + i + ')')
+    //is the seat occupied
+    if (!occupiedDesks.includes(i)) {
+      // Seat i is open!
+      seatCombo = [i, i + 1]
+      //is next seat open && seat on the left
+      if (!occupiedDesks.includes(i + 1) && !(i % 2 == 0)) {
+        // Next seat is open!
+
+        seatCombos.push(seatCombo)
+        // console.log('--    adding seatCombo [' + seatCombo + ']' + ' Now:' + seatCombos.length)
+      } else {
+        // console.log('--not adding seatCombo [' + seatCombo + ']' + ' Still:' + seatCombos.length)
+      }
+
+      seatCombo = [i, i + 2]
+
+      //is seat down open && not last seat
+      if (!occupiedDesks.includes(i + 2) && i < deskCount - 1) {
+        seatCombos.push(seatCombo)
+        // console.log('--    adding seatCombo [' + seatCombo + ']' + ' Now:' + seatCombos.length)
+      } else {
+        // console.log('seat down(' + (i + 2) + ') occupied')
+        // console.log('--not adding seatCombo [' + seatCombo + ']' + ' Still:' + seatCombos.length)
+      }
+    }
+    // else console.log('seat (' + i + ') occupied')
+  }
+
+  console.log('RESULTS ')
+  console.log('  Input:            : ' + JSON.stringify(arr))
+  console.log('  Seat Combinations : ' + JSON.stringify(seatCombos))
+  console.log('  Seat Combos       : ' + seatCombos.length + '\n')
+  return seatCombos.length + 'xXfiXmcX'
+}
+
+const arr1 = [12, 2, 6, 7, 11]
+// const arr2 = [8, 1, 8, 9]
+
+console.log(ArrayChallenge(arr1))
+
+// keep this function call here
+// console.log(ArrayChallenge(readline()))
